fd63d7ee2e76a339e6b3cf0260ff70d2f6de15fb
Loki2299 <151997715+Loki2299@users.noreply.github.com>
2026-05-10 01:27:46 +0630

    again

--- resources/views/products/_form.blade.php
+++ resources/views/products/_form.blade.php
@@ -1,5 +1,3 @@
-@csrf
-
 <label class="block">
     <span class="text-sm font-medium">Name</span>
     <input class="mt-1 w-full rounded border border-slate-300 px-3 py-2" name="name" value="{{ old('name', $product->name ?? '') }}"  maxlength="255">
